09558e4b5474d7c494e32750b69ff757f636c531
Anastasia <153524961+Fanatasia@users.noreply.github.com>
2023-12-11 17:08:38 +0200

    Update nastia.py

--- nastia.py
+++ nastia.py
@@ -3,15 +3,15 @@ from PyQt5.QtWidgets import*
 
 app=QApplication([])
 win=QWidget()
-
+#створення вікна
 win.resize(500,500)
 win.setWindowTitle("Тест 1#")
-
+#розмір вікна
 title=QLabel("Тестовий додаток")
-
+#назва  вікна
 line_h=QHBoxLayout()
 line_h.addWidget(title)
-
+#назва вікна2
 win.setLayout(line_h)
 
 win.show()
